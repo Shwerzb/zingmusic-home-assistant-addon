4543b496941af1094bf25426d9aab3c575415e7a
Shwerzb <153048058+Shwerzb@users.noreply.github.com>
2025-05-25 20:12:16 -0400

    Create __init__.py

---
+++ zingmusic/zingmusic_provider/__init__.py
@@ -0,0 +1,20 @@
+from music_assistant.server.models.music_provider import MusicProvider
+from music_assistant.server.providers.filesystem import FilesystemProvider
+from music_assistant.server.providers.radio_browser import RadioBrowserProvider
+from music_assistant.server.providers.shoutcast import ShoutcastProvider
+from music_assistant.server.providers.tunein import TuneInProvider
+from music_assistant.server.providers.qobuz import QobuzProvider
+from music_assistant.server.providers.spotify import SpotifyProvider
+from music_assistant.server.providers.ytmusic import YTMusicProvider
+from .zingmusic import ZingMusicProvider  # 👈 your provider
+
+PROVIDERS: list[type[MusicProvider]] = [
+    ZingMusicProvider,  # 👈 register here
+    FilesystemProvider,
+    RadioBrowserProvider,
+    ShoutcastProvider,
+    TuneInProvider,
+    # QobuzProvider,
+    # SpotifyProvider,
+    # YTMusicProvider,
+]
